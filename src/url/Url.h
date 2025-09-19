1a7144efe250956ab444f6a6093767d5889101d5
onizuka <aleksa.curcic.dev@gmail.com>
2025-09-19 10:36:24 +0200

    feature: Url as wrapper of string

--- src/url/Url.h
+++ src/url/Url.h
@@ -4,7 +4,23 @@
 #include <vector>
 
 class Url {
+private:
+    std::string m_url;
+
 public:
-    static std::vector<std::string> loadFromFile();
-    static std::vector<std::string> loadFromConsole();
+    Url(const std::string& url) : m_url(url) {}
+
+    const std::string& str() const { return m_url; }
+
+    bool empty() const { return m_url.empty(); }
+
+    bool isAbsolute() const { return m_url.find("http") == 0; }
+
+    bool operator==(const Url& other) const { return m_url == other.m_url; }
+
+    friend std::ostream& operator<<(std::ostream& out, const Url& url) {
+        return out << url.m_url;
+    }
 };
+
+using UrlList = std::vector<Url>;
